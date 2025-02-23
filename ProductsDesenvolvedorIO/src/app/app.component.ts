436e1f2977fc4f4e0743258dfea5aa3b46accb37
jtauanpm <jtauan10@gmail.com>
2025-02-23 00:46:42 -0300

    initial

---
+++ ProductsDesenvolvedorIO/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./navigation/home/home.component";
+import { NavbarComponent } from "./navigation/navbar/navbar.component";
+import { FooterComponent } from "./navigation/footer/footer.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [HomeComponent, NavbarComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styles: [],
+})
+export class AppComponent {
+  title = 'ProductsDesenvolvedorIO';
+}
