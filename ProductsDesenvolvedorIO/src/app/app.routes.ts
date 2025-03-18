cdbe147ad78a8d286e9fade2e7ad5cb5f0685635
jtauanpm <jtauan10@gmail.com>
2025-03-17 21:10:44 -0300

    add lazy loading for databinding component

--- ProductsDesenvolvedorIO/src/app/app.routes.ts
+++ ProductsDesenvolvedorIO/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './navigation/home/home.component';
 import { ContactComponent } from './institutional/contact/contact.component';
 import { AboutComponent } from './institutional/about/about.component';
-import { DataBindingComponent } from './demos/data-binding/data-binding.component';
 import { RXJSComponent } from './demos/rxjs/rxjs.component';
 import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
 
@@ -11,9 +10,11 @@ export const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "contact", component: ContactComponent },
   { path: "about", component: AboutComponent },
-  { path: "databinding", component: DataBindingComponent },
   { path: "rxjs", component: RXJSComponent },
   { path: "cadastro", component: CadastroComponent },
+  { path: "databinding",
+    loadComponent: () => import('./demos/data-binding/data-binding.component')
+    .then(dataBinding => dataBinding.DataBindingComponent)},
   { path: "products", 
     loadChildren: () => import('./products/product.routes')
     .then(routes => routes.PRODUCT_ROUTES) }
