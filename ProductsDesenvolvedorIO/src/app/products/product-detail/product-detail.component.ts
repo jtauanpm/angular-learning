5c6df7463143671747c9493e18566c6ee624ef1a
jtauanpm <jtauan10@gmail.com>
2025-03-18 20:54:25 -0300

    fixes

--- ProductsDesenvolvedorIO/src/app/products/product-detail/product-detail.component.ts
+++ ProductsDesenvolvedorIO/src/app/products/product-detail/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { ProductsService } from '../products.service';
 import { Product } from '../products.interface';
 import { CommonModule } from '@angular/common';
@@ -24,10 +24,9 @@ export class ProductDetailComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.subRoutes = this.activatedRoute.params.subscribe((params) => {
       this.productsService.getProductById(params["id"]).subscribe({
-        next: (product) => (this.product = product),
+        next: (product) => this.product = product,
         error: (err) => console.log(err),
       });
-      console.log(this.product);
     });
   }
 
--- ProductsDesenvolvedorIO/src/app/products/products.interface.ts
+++ ProductsDesenvolvedorIO/src/app/products/products.interface.ts
@@ -2,7 +2,7 @@ export interface Product {
     id: number;
     name: string;
     value: number;
-    promotional: string;
+    promotional: boolean;
     promotionalValue: string;
     image: string;
 };
