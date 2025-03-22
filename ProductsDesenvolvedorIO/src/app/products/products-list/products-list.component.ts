bc7b683a803dc768a7743dbc92f3b07b02264aba
jtauanpm <jtauan10@gmail.com>
2025-03-21 22:07:59 -0300

    using @ViewChild and @ViewChildren decorators

--- ProductsDesenvolvedorIO/src/app/products/products-list/products-list.component.ts
+++ ProductsDesenvolvedorIO/src/app/products/products-list/products-list.component.ts
@@ -1,20 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { ProductsService } from '../products.service';
 import { Product } from '../../interfaces/product.interface';
 import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { ProductPromotionalCountComponent } from '../product-promotional-count/product-promotional-count.component';
+import { fromEvent, Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-products-list',
-  imports: [CommonModule, ProductCardComponent, ProductPromotionalCountComponent],
+  selector: "app-products-list",
+  imports: [
+    CommonModule,
+    ProductCardComponent,
+    ProductPromotionalCountComponent,
+  ],
   providers: [ProductsService],
-  templateUrl: './products-list.component.html'
+  templateUrl: "./products-list.component.html",
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent implements OnInit, AfterViewInit {
   public products: Product[] = [];
 
-  constructor(private productsService: ProductsService) {  }
+  @ViewChild(ProductPromotionalCountComponent, { static: false }) promotionalCount!: ProductPromotionalCountComponent;
+  @ViewChild("title", { static: false }) titleComponent!: ElementRef;
+  @ViewChildren(ProductCardComponent) cards!: QueryList<ProductCardComponent>;
+
+  constructor(private productsService: ProductsService) {}
+
+  ngAfterViewInit(): void {
+    // ViewChild promotionalCount
+    setTimeout(() => console.log("produtos", this.promotionalCount.products), 1000);
+
+    // ViewChild titleComponent
+    let clickTitleObs: Observable<any> = fromEvent(
+      this.titleComponent.nativeElement,
+      "click"
+    );
+    clickTitleObs.subscribe(() => alert("Title click"));
+
+    // ViewChildren cards
+    setTimeout(() => {
+      this.cards.forEach(card => console.log("card:", card.product));
+      
+    }, 1000);
+  }
 
   ngOnInit(): void {
     this.productsService.getProducts()
