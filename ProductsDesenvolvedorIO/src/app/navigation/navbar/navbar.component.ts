6d697cd26ae8f299d826659d1726493826717e64
jtauanpm <jtauan10@gmail.com>
2025-03-22 08:55:09 -0300

    refactor navbar component

--- ProductsDesenvolvedorIO/src/app/navigation/navbar/navbar.component.ts
+++ ProductsDesenvolvedorIO/src/app/navigation/navbar/navbar.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
+import { NavbarItem } from '../../interfaces/navbar-item.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-navbar',
-  imports: [RouterLink],
-  templateUrl: './navbar.component.html'
+  selector: "app-navbar",
+  imports: [CommonModule, RouterLink, RouterModule],
+  templateUrl: "./navbar.component.html",
 })
 export class NavbarComponent {
-
+  public navbarItems: NavbarItem[] = [
+    { name: "Sobre", path: "/about", exact: true },
+    { name: "Cadastro", path: "/cadastro" },
+    { name: "Contato", path: "/contact" },
+    { name: "Produtos", path: "/products" },
+    { name: "Data Binding", path: "/databinding" },
+    { name: "RxJS", path: "/rxjs" }
+  ];
 }
