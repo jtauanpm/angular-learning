76d55b11ddb35caadabe1e75be68cfec13a8ab52
jtauanpm <jtauan10@gmail.com>
2025-03-23 20:54:03 -0300

    using submodule with children routes

--- ProductsDesenvolvedorIO/src/app/products/product.routes.ts
+++ ProductsDesenvolvedorIO/src/app/products/product.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from "@angular/router";
 import { ProductsListComponent } from "./products-list/products-list.component";
 import { ProductDetailComponent } from "./product-detail/product-detail.component";
+import { ProductAppComponent } from "./product.app.component";
 
 export const PRODUCT_ROUTES: Routes = [
-  { path: "", component: ProductsListComponent },
-  { path: ":id", component: ProductDetailComponent },
+  {
+    path: "",
+    component: ProductAppComponent,
+    children: [
+      { path: "", component: ProductsListComponent },
+      { path: ":id", component: ProductDetailComponent },
+    ],
+  },
 ];
