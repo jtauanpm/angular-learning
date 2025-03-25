4bcb6c8d5f01d58ec11befe129759e3afa3adcab
jtauanpm <jtauan10@gmail.com>
2025-03-24 22:20:43 -0300

    prducts service move

--- ProductsDesenvolvedorIO/src/app/products/services/products.service.ts
+++ ProductsDesenvolvedorIO/src/app/products/services/products.service.ts
@@ -1,17 +1,21 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { environment } from "../../../environment";
+import { environment } from "../../../../environment";
 import { Observable } from "rxjs";
-import { Product } from "../interfaces/product.interface";
+import { Product } from "../../interfaces/product.interface";
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class ProductsService {
   protected urlService: string = `${environment.gatewayUrl}/products`;
 
   constructor(private httpClient: HttpClient) {}
 
-  getProducts(): Observable<Product[]> {
-    return this.httpClient.get<Product[]>(this.urlService);
+  getProducts(state: string): Observable<Product[]> {
+    const url = state === "promotional"
+        ? `${this.urlService}?promotional=true`
+        : this.urlService;
+
+    return this.httpClient.get<Product[]>(url);
   }
 
   getProductById(id: number): Observable<Product> {
