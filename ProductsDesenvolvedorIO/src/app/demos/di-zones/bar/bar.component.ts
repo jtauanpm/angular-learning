ae182a390babd9a4ccf33854ccc254870644e689
jtauanpm <jtauan10@gmail.com>
2025-04-04 21:25:43 -0300

    ngZones processing outside angular zone

--- ProductsDesenvolvedorIO/src/app/demos/di-zones/bar/bar.component.ts
+++ ProductsDesenvolvedorIO/src/app/demos/di-zones/bar/bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Injector, OnInit } from '@angular/core';
+import { Component, Inject, Injector, NgZone, OnInit } from '@angular/core';
 import { BarFactory, BarService, BarServiceMock, BebidasService } from './bar.service';
 import { DI_ZONES_CONFIG, DiZonesConfig } from '../di-zones.config';
 import { HttpClient } from '@angular/common/http';
@@ -25,6 +25,7 @@ export class BarComponent implements OnInit {
 
   constructor(
     private barService: BarService,
+    private ngZone: NgZone,
     private bebidasService: BebidasService,
     @Inject(DI_ZONES_CONFIG) private zonesConfigByInjectionToken: DiZonesConfig,
     @Inject("DI_ZONES_CONFIG") private zonesConfigbyString: DiZonesConfig
@@ -38,5 +39,41 @@ export class BarComponent implements OnInit {
     this.tokenByUseFactoryInjection = this.barService.obterUnidade();
 
     this.bebidasByUseExisting = this.bebidasService.obterBebidas();
+  }
+
+  progress: number = 0;
+  label!: string;
+
+  // Loop inside the Angular zone
+  // so the UI DOES refresh after each setTimeout cycle
+  processWithinAngularZone() {
+    this.label = "inside";
+    this.progress = 0;
+    this._increaseProgress(() => console.log("Inside Done!"));
+  }
+
+  // Loop outside of the Angular zone
+  // so the UI DOES NOT refresh after each setTimeout cycle
+  processOutsideOfAngularZone() {
+    this.label = "outside";
+    this.progress = 0;
+    this.ngZone.runOutsideAngular(() => {
+      this._increaseProgress(() => {
+        // reenter the Angular zone and display done
+        this.ngZone.run(() => {
+          console.log("Outside Done!");
+        });
+      });
+    });
+  }
+  
+  _increaseProgress(doneCallback: () => void) {
+    this.progress += 1;
+    console.log(`Current progress: ${this.progress}%`);
+    if (this.progress < 100) {
+      window.setTimeout(() => this._increaseProgress(doneCallback), 10);
+    } else {
+      doneCallback();
+    }
   }
 }
