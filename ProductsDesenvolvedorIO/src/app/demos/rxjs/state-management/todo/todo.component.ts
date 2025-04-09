fffc276934911a1529d5e5e4f5d77cf7b845fb55
jtauanpm <jtauan10@gmail.com>
2025-04-09 18:59:54 -0300

    refactor todo component

--- ProductsDesenvolvedorIO/src/app/demos/rxjs/state-management/todo/todo.component.ts
+++ ProductsDesenvolvedorIO/src/app/demos/rxjs/state-management/todo/todo.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TasksIniciadasComponent } from './components/tasks-iniciadas/tasks-iniciadas.component';
+import { TasksFinalizadasComponent } from './components/tasks-finalizadas/tasks-finalizadas.component';
 
 @Component({
-  selector: 'app-todo-list',
-  templateUrl: './todo.component.html'
+  selector: "app-todo-list",
+  imports: [TasksComponent, TasksIniciadasComponent, TasksFinalizadasComponent],
+  templateUrl: "./todo.component.html"
 })
 export class TodoComponent {}
