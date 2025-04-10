ca6a70c99d4ed86e1cfac4e4c2bd5f910d6c626b
jtauanpm <jtauan10@gmail.com>
2025-04-10 20:43:53 -0300

    add todo listt component

---
+++ ProductsDesenvolvedorIO/src/app/demos/rxjs/state-management/todo/components/todo-list/todo-list.component.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+import { TodoTask } from '../../../../../../interfaces/todo-task.interface';
+
+@Component({
+  selector: 'todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['todo-list.component.css']  
+})
+export class ToDoListComponent {
+  public todoList = input.required<TodoTask[]>();
+}
