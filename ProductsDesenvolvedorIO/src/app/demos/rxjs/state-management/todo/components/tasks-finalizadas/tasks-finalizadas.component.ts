a14a10dbeaba89cfb36d0b4f87473d40dba43ec1
jtauanpm <jtauan10@gmail.com>
2025-04-25 21:52:08 -0300

    using store

--- ProductsDesenvolvedorIO/src/app/demos/rxjs/state-management/todo/components/tasks-finalizadas/tasks-finalizadas.component.ts
+++ ProductsDesenvolvedorIO/src/app/demos/rxjs/state-management/todo/components/tasks-finalizadas/tasks-finalizadas.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 import { TasksService } from '../../todo.service';
 import { ToDoListComponent } from '../todo-list/todo-list.component';
 import { CommonModule } from '@angular/common';
+import { Store } from '../../todo.store';
 
 @Component({
   selector: 'tasks-finalizadas',
@@ -15,9 +16,9 @@ export class TasksFinalizadasComponent implements OnInit {
 
   finalizados$!: Observable<any[]>;
 
-  constructor(private tasksService: TasksService) { }
+  constructor(private tasksService: TasksService, private store: Store) { }
 
   ngOnInit() {
-    this.finalizados$ = this.tasksService.getToDoList();      
+    this.finalizados$ = this.store.getTodoList().pipe(map(todolist => todolist.filter(task => task.finalizado)));      
   }
 }
