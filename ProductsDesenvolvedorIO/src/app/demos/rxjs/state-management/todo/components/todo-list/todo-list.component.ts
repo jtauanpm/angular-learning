a55af0206c414ad89ee3e4ab19ec850678190f58
jtauanpm <jtauan10@gmail.com>
2025-04-28 21:53:52 -0300

    add update task status on todo-list

--- ProductsDesenvolvedorIO/src/app/demos/rxjs/state-management/todo/components/todo-list/todo-list.component.ts
+++ ProductsDesenvolvedorIO/src/app/demos/rxjs/state-management/todo/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { Task } from '../../../../../../interfaces/task.interface';
 
 @Component({
@@ -8,4 +8,32 @@ import { Task } from '../../../../../../interfaces/task.interface';
 })
 export class ToDoListComponent {
   public todoList = input.required<Task[]>();
+  public toggle = output<any>();
+
+  public toggleItem(taskId: number, status: string){
+    const task = this.todoList()[taskId];
+
+    switch (status) {
+      case "iniciar":
+        task.finalizado = false;
+        task.iniciado = true;
+        break;
+      case "finalizar":
+        task.finalizado = true;
+        task.iniciado = false;
+        break;
+      case "retomar":
+        task.finalizado = false;
+        task.iniciado = true;
+        break;
+      case "cancelar":
+        task.finalizado = false;
+        task.iniciado = false;
+        break;
+    }
+
+    this.toggle.emit({
+      task: { ...task }
+    });
+  }
 }
