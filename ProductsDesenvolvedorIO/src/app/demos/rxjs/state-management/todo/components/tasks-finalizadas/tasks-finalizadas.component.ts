a276cae94060fb5f87c2867c9b46b9ce70420a3d
jtauanpm <jtauan10@gmail.com>
2025-04-29 19:16:25 -0300

    update task by event binding

--- ProductsDesenvolvedorIO/src/app/demos/rxjs/state-management/todo/components/tasks-finalizadas/tasks-finalizadas.component.ts
+++ ProductsDesenvolvedorIO/src/app/demos/rxjs/state-management/todo/components/tasks-finalizadas/tasks-finalizadas.component.ts
@@ -8,17 +8,22 @@ import { CommonModule } from '@angular/common';
 import { Store } from '../../todo.store';
 
 @Component({
-  selector: 'tasks-finalizadas',
+  selector: "tasks-finalizadas",
   imports: [CommonModule, ToDoListComponent],
-  templateUrl: './tasks-finalizadas.component.html'
+  templateUrl: "./tasks-finalizadas.component.html",
 })
 export class TasksFinalizadasComponent implements OnInit {
-
   finalizados$!: Observable<any[]>;
 
-  constructor(private tasksService: TasksService, private store: Store) { }
+  constructor(private tasksService: TasksService, private store: Store) {}
 
   ngOnInit() {
-    this.finalizados$ = this.store.getTodoList().pipe(map(todolist => todolist.filter(task => task.finalizado)));      
+    this.finalizados$ = this.store
+      .getTodoList()
+      .pipe(map((todolist) => todolist.filter((task) => task.finalizado)));
+  }
+
+  onToggle(event: any) {
+    this.tasksService.toggle(event);
   }
 }
