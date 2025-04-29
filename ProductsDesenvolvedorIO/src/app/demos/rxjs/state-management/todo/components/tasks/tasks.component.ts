0736131c193c36de7edfc15aec5ea505e468e2f8
jtauanpm <jtauan10@gmail.com>
2025-04-29 19:16:19 -0300

    update task by event binding

--- ProductsDesenvolvedorIO/src/app/demos/rxjs/state-management/todo/components/tasks/tasks.component.ts
+++ ProductsDesenvolvedorIO/src/app/demos/rxjs/state-management/todo/components/tasks/tasks.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { map, Observable, Subscription } from 'rxjs';
+import { Component, model, OnDestroy, OnInit, signal } from '@angular/core';
+import { first, map, Observable, Subscription } from 'rxjs';
 
 import { TasksService } from '../../todo.service';
 import { ToDoListComponent } from '../todo-list/todo-list.component';
 import { CommonModule } from '@angular/common';
 import { Task } from '../../../../../../interfaces/task.interface';
 import { Store } from '../../todo.store';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: "tasks",
-  imports: [ToDoListComponent, 
-    CommonModule
-  ],
+  imports: [ToDoListComponent, CommonModule, FormsModule],
   // providers: [TasksService],
   templateUrl: "./tasks.component.html",
 })
 export class TasksComponent implements OnInit, OnDestroy {
   todolist$!: Observable<Task[]>;
+  newTaskField = signal<string>("");
   subscription!: Subscription;
 
   constructor(private taskService: TasksService, private store: Store) {}
@@ -26,9 +26,27 @@ export class TasksComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.todolist$ = this.store.getTodoList().pipe(map(todolist => todolist.filter(task => !task.finalizado && !task.iniciado)));
+    this.todolist$ = this.store
+      .getTodoList()
+      .pipe(
+        map((todolist) =>
+          todolist.filter((task) => !task.iniciado && !task.finalizado)
+        )
+      );
 
     // utilizado para popular a store por meio do serviço
     this.subscription = this.taskService.getToDoList().subscribe();
+  }
+
+  onToggle(event: any) {
+    this.taskService.toggle(event);
+  }
+
+  addTask() {
+    if (!this.newTaskField().trim()) 
+      return;
+
+    console.log(this.newTaskField());
+    this.taskService.createTask(this.newTaskField());
   }
 }
