821db7c92bacb1c74dc3be8cdc6433b2c6e9b080
jtauanpm <jtauan10@gmail.com>
2025-05-21 19:58:13 -0300

    add cpf validator and ngx mask

---
+++ DevIO.FinalProject/DevIO.FinalProject-frontend/src/app/utils/cpf-validator.ts
@@ -0,0 +1,46 @@
+import { AbstractControl, ValidationErrors } from "@angular/forms";
+
+export function CpfValidator(control: AbstractControl): ValidationErrors | null {
+  const cpf = control.value?.replace(/\D/g, "");
+  if(!cpf) return null;
+
+  if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) {
+    return { invalidCPF: true };
+  }
+
+  let sum = 0;
+  let rest;
+
+  for (let i = 1; i <= 9; i++) {
+    sum += parseInt(cpf.substring(i - 1, i), 10) * (11 - i);
+  }
+
+  rest = (sum * 10) % 11;
+
+  if (rest === 10 || rest === 11) {
+    rest = 0;
+  }
+
+  if (rest !== parseInt(cpf.substring(9, 10), 10)) {
+    return { invalidCPF: true };
+  }
+
+  sum = 0;
+
+  for (let i = 1; i <= 10; i++) {
+    sum += parseInt(cpf.substring(i - 1, i), 10) * (12 - i);
+  }
+
+  rest = (sum * 10) % 11;
+
+  if (rest === 10 || rest === 11) {
+    rest = 0;
+  }
+
+  if (rest !== parseInt(cpf.substring(10, 11), 10)) {
+    return { invalidCPF: true };
+  }
+
+  return null;
+}
+
