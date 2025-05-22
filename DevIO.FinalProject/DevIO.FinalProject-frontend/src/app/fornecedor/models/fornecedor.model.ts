1f52357043183adb9900c28c695bfa5b256efaf8
jtauanpm <jtauan10@gmail.com>
2025-05-22 20:11:58 -0300

    new models

---
+++ DevIO.FinalProject/DevIO.FinalProject-frontend/src/app/fornecedor/models/fornecedor.model.ts
@@ -0,0 +1,11 @@
+import { Endereco } from './endereco.model';
+
+export interface Fornecedor {
+    id: string;
+    nome: string;
+    documento: string;
+    ativo: boolean;
+    tipoFornecedor: number;
+    endereco: Endereco;
+}
+
