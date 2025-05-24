43387a117ff18792c51df6a4b60fa39a00f54e8c
jtauanpm <jtauan10@gmail.com>
2025-05-24 00:00:26 -0300

    lista fornecedores

---
+++ DevIO.FinalProject/DevIO.FinalProject-frontend/src/app/fornecedor/lista/lista.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Fornecedor } from '../models/fornecedor.model';
+import { FornecedorService } from '../services/fornecedor.service';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class ListaComponent implements OnInit {
+
+  public fornecedores!: Fornecedor[];
+  errorMessage!: string;
+
+  constructor(private fornecedorService: FornecedorService) { }
+
+  ngOnInit(): void {
+    this.fornecedorService.obterTodos()
+      .subscribe(
+        fornecedores => this.fornecedores = fornecedores,
+        error => this.errorMessage);
+  }
+}
