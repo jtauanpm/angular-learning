96bfca966222b53f731fe7071ab24ed2063b032b
jtauanpm <jtauan10@gmail.com>
2025-05-24 00:00:09 -0300

    fornecedor

---
+++ DevIO.FinalProject/DevIO.FinalProject-frontend/src/app/fornecedor/fornecedor.app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'fornecedor-app-root',
+  template: '<router-outlet></router-outlet>',
+  imports: [RouterModule]
+})
+export class FornecedorAppComponent { }
---
+++ DevIO.FinalProject/DevIO.FinalProject-frontend/src/app/fornecedor/services/fornecedor.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { Observable } from "rxjs";
+import { catchError, map } from "rxjs/operators";
+
+import { Fornecedor } from '../models/fornecedor.model';
+import { BaseService } from "../../services/base-api.service";
+import { CepConsulta } from "../models/endereco.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class FornecedorService extends BaseService {
+
+    fornecedor!: Fornecedor;
+
+    constructor(private http: HttpClient) { super() 
+    
+        this.fornecedor = {
+            nome: "Teste Fake",
+            documento: "32165498754",
+            ativo: true,
+            tipoFornecedor: 1
+        } as Fornecedor;
+    }
+
+    obterTodos(): Observable<Fornecedor[]> {
+        return this.http
+            .get<Fornecedor[]>(this.UrlServiceV1 + "/fornecedores", this.obterAuthHeaderJson())
+            .pipe(catchError(super.serviceError));
+    }
+
+    obterPorId(id: string): Observable<Fornecedor> {
+        return this.http
+            .get<Fornecedor>(this.UrlServiceV1 + "/fornecedores/" + id, this.obterAuthHeaderJson())
+            .pipe(catchError(super.serviceError));
+    }
+
+    novoFornecedor(fornecedor: Fornecedor): Observable<Fornecedor> {
+        fornecedor.tipoFornecedor = Number(fornecedor.tipoFornecedor);
+        return this.http
+        .post<Fornecedor[]>(this.UrlServiceV1 + "/fornecedores", fornecedor, super.obterAuthHeaderJson())
+        .pipe(
+            map(super.extractData),
+            catchError(super.serviceError));
+    }
+
+    atualizarFornecedor(fornecedor: Fornecedor): Observable<Fornecedor> {
+        return new Observable<Fornecedor>();
+    }
+
+    excluirFornecedor(id: string): Observable<Fornecedor> {
+        return new Observable<Fornecedor>();
+    }
+    
+    consultarCep(cep: string): Observable<CepConsulta> {
+        return this.http
+            .get<CepConsulta>(`https://viacep.com.br/ws/${cep}/json/`)
+            .pipe(catchError(super.serviceError));
+    }
+}
