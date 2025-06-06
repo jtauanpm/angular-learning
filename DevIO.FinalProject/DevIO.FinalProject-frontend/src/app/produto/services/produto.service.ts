66b01ab7c1eaf4f32c083c7f3f72444738def604
jtauanpm <jtauan10@gmail.com>
2025-06-05 22:43:12 -0300

    fix produtos routes

--- DevIO.FinalProject/DevIO.FinalProject-frontend/src/app/produto/services/produto.service.ts
+++ DevIO.FinalProject/DevIO.FinalProject-frontend/src/app/produto/services/produto.service.ts
@@ -6,26 +6,26 @@ import { catchError, map } from "rxjs/operators";
 import { Produto, Fornecedor } from '../models/produto';
 import { BaseService } from "../../services/base-api.service";
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class ProdutoService extends BaseService {
 
     constructor(private http: HttpClient) { super() }
 
     obterTodos(): Observable<Produto[]> {
         return this.http
-            .get<Produto[]>(this.UrlServiceV1 + "produtos", super.obterAuthHeaderJson())
+            .get<Produto[]>(this.UrlServiceV1 + "/produtos", super.obterAuthHeaderJson())
             .pipe(catchError(super.serviceError));
     }
 
     obterPorId(id: string): Observable<Produto> {
         return this.http
-            .get<Produto>(this.UrlServiceV1 + "produtos/" + id, super.obterAuthHeaderJson())
+            .get<Produto>(this.UrlServiceV1 + "/produtos/" + id, super.obterAuthHeaderJson())
             .pipe(catchError(super.serviceError));
     }
 
     novoProduto(produto: Produto): Observable<Produto> {
         return this.http
-            .post(this.UrlServiceV1 + "produtos", produto, super.obterAuthHeaderJson())
+            .post(this.UrlServiceV1 + "/produtos", produto, super.obterAuthHeaderJson())
             .pipe(
                 map(super.extractData),
                 catchError(super.serviceError));
@@ -33,7 +33,7 @@ export class ProdutoService extends BaseService {
 
     atualizarProduto(produto: Produto): Observable<Produto> {
         return this.http
-            .put(this.UrlServiceV1 + "produtos/" + produto.id, produto, super.obterAuthHeaderJson())
+            .put(this.UrlServiceV1 + "/produtos/" + produto.id, produto, super.obterAuthHeaderJson())
             .pipe(
                 map(super.extractData),
                 catchError(super.serviceError));
@@ -41,7 +41,7 @@ export class ProdutoService extends BaseService {
 
     excluirProduto(id: string): Observable<Produto> {
         return this.http
-            .delete(this.UrlServiceV1 + "produtos/" + id, super.obterAuthHeaderJson())
+            .delete(this.UrlServiceV1 + "/produtos/" + id, super.obterAuthHeaderJson())
             .pipe(
                 map(super.extractData),
                 catchError(super.serviceError));
