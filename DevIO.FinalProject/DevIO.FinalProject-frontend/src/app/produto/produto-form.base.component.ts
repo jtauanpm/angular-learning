6d46e0fae98a37cf3877de3ef464a5f1831cc9c7
jtauanpm <jtauan10@gmail.com>
2025-07-09 18:59:10 -0300

    generic produto form

---
+++ DevIO.FinalProject/DevIO.FinalProject-frontend/src/app/produto/produto-form.base.component.ts
@@ -0,0 +1,43 @@
+import { ElementRef } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+import { FormBaseComponent } from "../base-components/form-base.component";
+import { Fornecedor, Produto } from "./models/produto";
+
+export abstract class ProdutoFormBaseComponent extends FormBaseComponent {
+    protected produto!: Produto;
+    fornecedores!: Fornecedor[];
+    errors: any[] = [];
+    produtoForm!: FormGroup;
+
+    constructor() {
+        super();
+
+        this.validationMessages = {
+            fornecedorId: {
+                required: 'Escolha um fornecedor',
+            },
+            nome: {
+                required: 'Informe o Nome',
+                minlength: 'Mínimo de 2 caracteres',
+                maxlength: 'Máximo de 200 caracteres'
+            },
+            descricao: {
+                required: 'Informe a Descrição',
+                minlength: 'Mínimo de 2 caracteres',
+                maxlength: 'Máximo de 1000 caracteres'
+            },
+            imagem: {
+                required: 'Informe a Imagem',
+            },
+            valor: {
+                required: 'Informe o Valor',
+            }
+        };
+
+        super.configurarMensagensValidacaoBase(this.validationMessages);
+    }
+
+    protected configurarValidacaoFormulario(formInputElements: ElementRef[]) {
+        super.configurarValidacaoFormularioBase(formInputElements, this.produtoForm);
+    }
+}
