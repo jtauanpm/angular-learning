3ab2a38b6f203dbc8845cf1ce0edfb6a6bdf194b
Jonathan Tauan Pereira Maia <jtauan10@gmail.com>
2025-07-13 23:01:57 -0300

    Update environment.ts

--- DevIO.FinalProject/DevIO.FinalProject-frontend/src/environments/environment.ts
+++ DevIO.FinalProject/DevIO.FinalProject-frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     gatewayUrl: "http://localhost:8080/api",
-    googleMapsApiKey: 'AIzaSyBfv4c4Ds-DdNt2S0CSI8lily6uQV2wEVY',
+    googleMapsApiKey: '',
     imagesUrl: "http://localhost:8080",
 };
